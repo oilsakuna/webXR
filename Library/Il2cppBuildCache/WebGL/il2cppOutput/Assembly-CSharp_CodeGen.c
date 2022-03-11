c8c1a128d298d9aa62192e3cc2bef4e4757150b0
oilsakuna <oil.iop577@gmail.com>
2022-03-11 22:16:38 +0700

    first commit

---
+++ Library/Il2cppBuildCache/WebGL/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,87 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CameraControl::Start()
+extern void CameraControl_Start_m77CCD413451A457E13B0F9D4EBB0D76633CEC5C1 (void);
+// 0x00000002 System.Void CameraControl::Update()
+extern void CameraControl_Update_m4BD3B811296B1A0E5C8FF3148DC11622A6430F6F (void);
+// 0x00000003 System.Void CameraControl::.ctor()
+extern void CameraControl__ctor_m67DAD57065E2F985D5F050A4DDD3DB9B95548100 (void);
+// 0x00000004 System.Void itemControl::Start()
+extern void itemControl_Start_mFA0DDF49622CB916283A5261ABF92E6C7CCA28AB (void);
+// 0x00000005 System.Void itemControl::Update()
+extern void itemControl_Update_mE6C67878473AE0B8B68F972C9DC2BD9D4241F1B9 (void);
+// 0x00000006 System.Void itemControl::.ctor()
+extern void itemControl__ctor_mF1CDF244606336FD0C82A790CE27C5F69C47A0E7 (void);
+// 0x00000007 System.Void playerControl::Start()
+extern void playerControl_Start_m79478EFE9B7FB692A35A44758A032EEAB36B2479 (void);
+// 0x00000008 System.Void playerControl::playerMovement(System.Single)
+extern void playerControl_playerMovement_mE1EC20B76DD133AE7F1D3563A347B14DD2A35D09 (void);
+// 0x00000009 System.Void playerControl::OnTriggerEnter(UnityEngine.Collider)
+extern void playerControl_OnTriggerEnter_mE33862BE0C980EC8D944DFB8D4E4A9B77AC8B9C4 (void);
+// 0x0000000A System.Void playerControl::Update()
+extern void playerControl_Update_mB0198FAC403D75B14E36B61FB6AD3F70AB3854B6 (void);
+// 0x0000000B System.Void playerControl::.ctor()
+extern void playerControl__ctor_m8AAF24DEBD6672820A41122CDD1A459AC76B46E8 (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	CameraControl_Start_m77CCD413451A457E13B0F9D4EBB0D76633CEC5C1,
+	CameraControl_Update_m4BD3B811296B1A0E5C8FF3148DC11622A6430F6F,
+	CameraControl__ctor_m67DAD57065E2F985D5F050A4DDD3DB9B95548100,
+	itemControl_Start_mFA0DDF49622CB916283A5261ABF92E6C7CCA28AB,
+	itemControl_Update_mE6C67878473AE0B8B68F972C9DC2BD9D4241F1B9,
+	itemControl__ctor_mF1CDF244606336FD0C82A790CE27C5F69C47A0E7,
+	playerControl_Start_m79478EFE9B7FB692A35A44758A032EEAB36B2479,
+	playerControl_playerMovement_mE1EC20B76DD133AE7F1D3563A347B14DD2A35D09,
+	playerControl_OnTriggerEnter_mE33862BE0C980EC8D944DFB8D4E4A9B77AC8B9C4,
+	playerControl_Update_mB0198FAC403D75B14E36B61FB6AD3F70AB3854B6,
+	playerControl__ctor_m8AAF24DEBD6672820A41122CDD1A459AC76B46E8,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	1181,
+	1181,
+	1181,
+	1181,
+	1181,
+	1181,
+	1181,
+	1025,
+	1006,
+	1181,
+	1181,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ Library/Il2cppBuildCache/WebGL/il2cppOutput/Il2CppReversePInvokeWrapperTable.cpp
@@ -0,0 +1,121 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <stdint.h>
+#include <limits>
+
+
+
+// System.Void
+struct Void_t700C6383A2A510C2CF4DD86DABD5CA9FF70ADAC5;
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.Object
+
+
+// System.ValueType
+struct ValueType_tDBF999C1B75C48C68621878250DBF6CDBCF51E52  : public RuntimeObject
+{
+public:
+
+public:
+};
+
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_tDBF999C1B75C48C68621878250DBF6CDBCF51E52_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_tDBF999C1B75C48C68621878250DBF6CDBCF51E52_marshaled_com
+{
+};
+
+// System.IntPtr
+struct IntPtr_t 
+{
+public:
+	// System.Void* System.IntPtr::m_value
+	void* ___m_value_0;
+
+public:
+	inline static int32_t get_offset_of_m_value_0() { return static_cast<int32_t>(offsetof(IntPtr_t, ___m_value_0)); }
+	inline void* get_m_value_0() const { return ___m_value_0; }
+	inline void** get_address_of_m_value_0() { return &___m_value_0; }
+	inline void set_m_value_0(void* value)
+	{
+		___m_value_0 = value;
+	}
+};
+
+struct IntPtr_t_StaticFields
+{
+public:
+	// System.IntPtr System.IntPtr::Zero
+	intptr_t ___Zero_1;
+
+public:
+	inline static int32_t get_offset_of_Zero_1() { return static_cast<int32_t>(offsetof(IntPtr_t_StaticFields, ___Zero_1)); }
+	inline intptr_t get_Zero_1() const { return ___Zero_1; }
+	inline intptr_t* get_address_of_Zero_1() { return &___Zero_1; }
+	inline void set_Zero_1(intptr_t value)
+	{
+		___Zero_1 = value;
+	}
+};
+
+
+// System.Void
+struct Void_t700C6383A2A510C2CF4DD86DABD5CA9FF70ADAC5 
+{
+public:
+	union
+	{
+		struct
+		{
+		};
+		uint8_t Void_t700C6383A2A510C2CF4DD86DABD5CA9FF70ADAC5__padding[1];
+	};
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_OSSpecificSynchronizationContext_InvocationEntry_m0045E44F7E960D6B4A864D5206D4116249C09BB0(intptr_t ___arg0);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_WebXRSubsystem_OnEndXR_m619B5F929D1B8F67A360917710695E1474E03F14();
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_WebXRSubsystem_OnInputProfiles_m13B2017F20BC87FB5A6652011A0BA662CFD7F26B(char* ___json0);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_WebXRSubsystem_OnStartAR_mCD56D3757C78CD5745E78516435B8036BFF851B7(int32_t ___viewsCount0, float ___left_x1, float ___left_y2, float ___left_w3, float ___left_h4, float ___right_x5, float ___right_y6, float ___right_w7, float ___right_h8);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_WebXRSubsystem_OnStartVR_mBD08B936DAEAEF64B167868B8E75F837B6AAFD76(int32_t ___viewsCount0, float ___left_x1, float ___left_y2, float ___left_w3, float ___left_h4, float ___right_x5, float ___right_y6, float ___right_w7, float ___right_h8);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_WebXRSubsystem_OnXRCapabilities_mFB0D9BE0B209BB2D46B00B8BDAAA4809D5C2AFFE(int32_t ___isARSupported0, int32_t ___isVRSupported1);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_WebXRSubsystem_UpdateVisibilityState_m315E0A2C9F0B076FE8B18D2898B4BAB50A324D39(int32_t ___visibilityState0);
+
+
+IL2CPP_EXTERN_C const Il2CppMethodPointer g_ReversePInvokeWrapperPointers[];
+const Il2CppMethodPointer g_ReversePInvokeWrapperPointers[7] = 
+{
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OSSpecificSynchronizationContext_InvocationEntry_m0045E44F7E960D6B4A864D5206D4116249C09BB0),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_WebXRSubsystem_OnEndXR_m619B5F929D1B8F67A360917710695E1474E03F14),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_WebXRSubsystem_OnInputProfiles_m13B2017F20BC87FB5A6652011A0BA662CFD7F26B),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_WebXRSubsystem_OnStartAR_mCD56D3757C78CD5745E78516435B8036BFF851B7),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_WebXRSubsystem_OnStartVR_mBD08B936DAEAEF64B167868B8E75F837B6AAFD76),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_WebXRSubsystem_OnXRCapabilities_mFB0D9BE0B209BB2D46B00B8BDAAA4809D5C2AFFE),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_WebXRSubsystem_UpdateVisibilityState_m315E0A2C9F0B076FE8B18D2898B4BAB50A324D39),
+};
